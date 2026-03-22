82de61f7ce6739669154b018df81fe6bc30f8a3d
Harshit <harshitbishtuni@gamil.com>
2026-03-22 09:23:25 +0530

    fixing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <h1>this is harshit bisht</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
